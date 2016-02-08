ea8bfca96a51bd18a0f23a76fb20b4a76493d9bf
Noah Scholfield <noah.scholfield@gmail.com>
2016-02-08 14:30:21 -0500

    Re-enable RedisStore

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var app = express();
   saveUninitialized: true,
   cookie: { secure: true }
 }));*/
-app.use(session({secret: 'jesus'}));
+app.use(session({store: new RedisStore({url: process.env.REDIS_URL}), secret: 'jesus'}));
 app.set('port', process.env.PORT || 4000);
 app.set('view engine', 'ejs');
 app.set('trust proxy');
