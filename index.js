680199ba2afc0b1b4647b3a816d75bca18c5e508
Noah Scholfield <noah.scholfield@gmail.com>
2016-02-08 15:36:15 -0500

    Remove secure cookie

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var formidable = require('formidable');
 var bcrypt = require('bcrypt');
 var app = express();
 
-app.use(session({ store: new RedisStore({url: process.env.REDIS_URL}), secret: process.env.SESSION_SECRET, resave: true, cookie: { secure: true } }));
+app.use(session({ store: new RedisStore({url: process.env.REDIS_URL}), secret: process.env.SESSION_SECRET, resave: true }));
 app.set('port', process.env.PORT || 4000);
 app.set('view engine', 'ejs');
 app.set('trust proxy');
